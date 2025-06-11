f00a7d15e188d1812f22c6219c1b508e453ba0d3
hmmhmmhm <hmmhmmhm@naver.com>
2025-06-11 22:16:34 +0900

    feat: 프리텐다드GOV 폰트 적용

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,19 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
 import "./globals.css";
+import { cn } from "@/lib/utils";
 
 export const metadata: Metadata = {
   title: "대한민국 대통령실",
   description:
     "'국민이 주인인 정부' , '국민과 함께하는 국정'을 실현하기 위한 정부",
 };
+
+const pretendard = localFont({
+  src: "./fonts/PretendardGOVVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+});
 
 export default function RootLayout({
   children,
@@ -14,7 +22,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="ko">
-      <body className="antialiased">{children}</body>
+      <body className={cn("antialiased", pretendard.className)}>
+        {children}
+      </body>
     </html>
   );
 }
